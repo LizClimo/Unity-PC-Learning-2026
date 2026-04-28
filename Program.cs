ab33e4b4a9c70bbe11c275ce0cf278ea9370b5c7
LizClimo <65879334+LizClimo@users.noreply.github.com>
2026-04-28 16:15:40 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("QQ:");
+            Console.Write("377142732");
+            
+        }
+    }
+}
